631d2cd916c8afcd34535ce986e2d009830ed518
Adrian <144566998+Adrian-Augusto@users.noreply.github.com>
2025-05-22 16:34:23 -0300

    First

---
+++ src/test/java/AdBot/ad/AdrianApplicationTests.java
@@ -0,0 +1,13 @@
+package AdBot.ad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdrianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
